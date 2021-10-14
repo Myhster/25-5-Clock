a196115413e531212f022933e6e9aa2751638d9e
Your Name <wertzuio987@web.de>
2021-10-14 18:18:48 +0200

    only one test fails + audio tests

--- src/Clock/Clock.js
+++ src/Clock/Clock.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Clock() {
   const [breakCounter, setBreakCounter] = useState(5);
@@ -6,6 +6,7 @@ function Clock() {
   const [timeSec, setTimeSec] = useState(sessionCounter * 60);
   const [timeString, setTimeString] = useState('25:00');
   const [intervalId, setIntervalId] = useState(0);
+  const [breakTime, setBreakTime] = useState(false);
 
   const decreasePause = () => {
     if (breakCounter > 1) setBreakCounter(breakCounter - 1);
@@ -18,18 +19,24 @@ function Clock() {
   const decreaseSession = () => {
     if (sessionCounter > 1) {
       setSessionCounter(sessionCounter - 1);
-      setTimeSec(sessionCounter * 60);
+      setTimeSec(sessionCounter * 60 - 60);
     }
   };
 
   const increaseSession = () => {
-    if (sessionCounter < 60) setSessionCounter(sessionCounter + 1);
+    if (sessionCounter < 60) {
+      setSessionCounter(sessionCounter + 1);
+      setTimeSec(sessionCounter * 60 + 60);
+    }
   };
 
   const resetAll = () => {
+    clearInterval(intervalId);
+    setIntervalId(0);
     setBreakCounter(5);
     setSessionCounter(25);
     setTimeSec(1500);
+    setBreakTime(false);
   };
   //-------------------timer-logic--------------------------
 
@@ -45,15 +52,38 @@ function Clock() {
     setIntervalId(newIntervalId);
   };
 
+  //-----------------------------Use-Effect---------------------------
   useEffect(() => {
-    let displayTime = new Date(0);
-    displayTime.setSeconds(timeSec);
-    setTimeString(displayTime.toISOString().substr(14, 5));
-    console.log(timeString);
+    let minutes = Math.floor(timeSec / 60);
+    let seconds = timeSec % 60;
+    if (timeSec < 0) {
+      if (breakTime === false) {
+        setTimeSec(breakCounter * 60);
+        setBreakTime(true);
+        console.log(breakTime);
+      } else {
+        setTimeSec(sessionCounter * 60);
+        setBreakTime(false);
+        console.log(breakTime);
+      }
+    }
+    if (minutes < 10) {
+      if (seconds < 10) {
+        setTimeString(`0${minutes}:0${seconds}`);
+      } else {
+        setTimeString(`0${minutes}:${seconds}`);
+      }
+    } else {
+      if (seconds < 10) {
+        setTimeString(`${minutes}:0${seconds}`);
+      } else {
+        setTimeString(`${minutes}:${seconds}`);
+      }
+    }
 
     return () => {};
   }, [breakCounter, sessionCounter, timeSec, timeString]);
-
+  //-------------------------------------------------------------------
   return (
     <div>
       <h1 data-testid='header'>My Clock 1</h1>
@@ -73,7 +103,7 @@ function Clock() {
       </button>
       <div id='break-length'>{breakCounter}</div>
       <div id='session-length'>{sessionCounter}</div>
-      <div id='timer-label'>Session</div>
+      <div id='timer-label'>{breakTime ? 'Break' : 'Session'}</div>
       <div id='time-left'>{timeString}</div>
       <button id='start_stop' onClick={countdown}>
         {intervalId ? 'Stop Timer' : 'Start Timer'}
